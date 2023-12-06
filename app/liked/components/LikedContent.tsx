a17a622c9b7e0459fa2890d92f606e85ad10f91b
lurgi <wahoo9040@naver.com>
2023-12-06 11:20:52 +0900

    feat(like): 노래 좋아요

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -1,22 +1,34 @@
 "use client";
 
-import { Song } from "@/types/types";
-
 import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
+import { useUser } from "@/hooks/useUser";
+import { Song } from "@/types/types";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-interface SearchContentProps {
+interface LikeContentProps {
   songs: Song[];
 }
 
-const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
+const LikedContent: React.FC<LikeContentProps> = ({ songs }) => {
+  const router = useRouter();
+  const { isLoading, user } = useUser();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/");
+    }
+  }, [isLoading, user, router]);
+
   if (songs.length === 0) {
     return (
       <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
-        No songs Found.
+        No Liked songs.
       </div>
     );
   }
+
   return (
     <div className="flex flex-col gap-y-2 w-full px-6">
       {songs.map((song) => (
@@ -31,4 +43,4 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
   );
 };
 
-export default SearchContent;
+export default LikedContent;
